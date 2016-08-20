da0b51add7f8ebddbdc8d60a0862d5548e16b21f
michael_afanasiev <mafanasiev@gmail.com>
2016-08-20 19:17:43 +0200

    Added parse_downloaded_file.
    Given a downloaded set of articles, we can now parse the file later and
    extract a named tuple containing the body and authorship information.

--- sentiment/sentiment.py
+++ sentiment/sentiment.py
@@ -1,9 +1,14 @@
 import io
+import re
 
 import click
+import pickle
 import requests
 from bs4 import BeautifulSoup
 from newspaper import Article
+from collections import namedtuple
+
+SentimentalArticle = namedtuple('SentimentalArticle', 'author title body')
 
 
 @click.group()
@@ -17,18 +22,60 @@ def cli():
 
     pass
 
+
 @cli.command()
 @click.option('--url', help='Search URL returned by a Google News query', type=str, required=True)
-def get_articles(url):
+@click.option('--output-file', help='Write articles to this file.', type=click.Path(writable=True),
+              default='output.txt')
+def get_articles(url, output_file):
     """
     Search, query, and download articles from the web.
     :param url: URL string from a Google News query.
     """
 
-    get_articles_run(url)
+    get_articles_run(url, output_file)
 
 
-def get_articles_run(url):
+@cli.command()
+@click.option('--input-file', help='Previously downloaded txt file',
+              type=click.Path(exists=True, readable=True), required=True)
+def get_sentiment(input_file):
+    """
+    Perform sentiment analysis on downloaded files.
+    :param input_file: Result of get_articles.
+    :return:
+    """
+
+    parse_downloaded_file(input_file)
+
+
+def parse_downloaded_file(input_file):
+    """
+    Open a previously downloaded news archive, and split into articles.
+    :param input_file: Path to downloaded file.
+    :return: List of named tuples containing SentimentalArticles.
+    """
+
+    with io.open(input_file, 'r', encoding='utf-8') as fh:
+        lines = fh.read()
+
+    body, header = [], []
+    segments = lines.split('-' * 80)
+    for i, s in enumerate(segments):
+        # Even numbers are article bodies
+        if i != 0 and not i % 2:
+            body.append(s)
+        # Odd numbers are the headers.
+        elif i % 2:
+            clean = re.split('AUTHORS: |TITLE: ', s.replace('\n', '').strip())[1:]
+            header.append({'AUTHORS': clean[0].strip(),
+                           'TITLE': clean[1].strip()})
+
+    return [SentimentalArticle(author=h['AUTHORS'], title=h['TITLE'], body=b)
+            for h, b in zip(header, body)]
+
+
+def get_articles_run(url, output_file):
     """
     Search, query, and download articles from the web.
     """
@@ -65,7 +112,7 @@ def get_articles_run(url):
                  'additional': article.meta_data})
 
     # Write formatted output to file
-    with io.open('output.txt', 'w', encoding='utf-8') as fh:
+    with io.open(output_file, 'w', encoding='utf-8') as fh:
         for article in full_text:
             # Header
             fh.write('-' * 80 + '\n')
--- tests/test_all.py
+++ tests/test_all.py
@@ -1,16 +1,21 @@
 import io
 import os
 
+import pickle
 from sentiment import sentiment
 
 
 def test_get_articles():
+    """
+    Test whether or not we can download a known set of files. Note that this will probably fail once google news is
+    updated.
+    """
     # Path to first page of Sault articles.
     test_url = u'https://www.google.com/search?cf=all&hl=en&pz=1&ned=ca&tbm=nws&gl=ca&as_q' \
                '=Makayla%20Sault&as_occt=any&tbs=ar%3A1&authuser=0'.encode('utf-8')
 
     # Get test Sault articles.
-    sentiment.get_articles_run(test_url)
+    sentiment.get_articles_run(test_url, 'output.txt')
 
     # Ensure that it is the same as that saved.
     with io.open(os.path.join('tests/data/trial_output.txt'), 'r', encoding='utf-8') as fh:
@@ -20,3 +25,12 @@ def test_get_articles():
     assert actual == ideal
     os.remove(os.path.join('output.txt'))
 
+
+def test_parse_files():
+    """
+    Test whether we can properly parse a downloaded text file.
+    :return:
+    """
+    with io.open('tests/data/trial_parse.pkl', 'rb') as fh:
+        ideal = pickle.load(fh)
+    assert sentiment.parse_downloaded_file(os.path.join('./tests/data/trial_output.txt')) == ideal
